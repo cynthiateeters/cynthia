302c4002aa059939746fff6bf3a1668af8b289c9
cynthia <glean@cynthiateeters.com>
2021-11-03 23:29:41 -0400

    Add colors

--- index.js
+++ index.js
@@ -2,25 +2,28 @@
 
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk');
 
+const log = console.log;
+const twitter = chalk.hex(`1da1f2`).bgWhite.bold.inverse;
+const github = chalk.hex(`6cc644`).bgWhite.bold.inverse;
+const website = chalk.hex(`c9510c`).bgWhite.bold.inverse;
 
 welcome({
-  title: pkgJSON.name,
+  title: ` Cynthia Teeters `,
   tagLine: `Hey, nice to meet you`,
   description: pkgJSON.description,
   version: pkgJSON.version,
-  bgColor: `#FADC00`,
-  color: `#000000`,
+  bgColor: `#fcffa4`,
+  color: `white`,
   bold: true,
   clear: true,
 });
 
-console.log(`
-Hi, I'm Cynthia Teeters
+console.log(`${chalk.italic(`🎓 Adjunct Assistant Professor - Raritan Valley Community College (she/her)
+🏫 I teach web development and JavaScript`)}
 
-🎓 Adjunct Assistant Professor - Raritan Valley Community College (she/her)
-🏫 I teach web development and JavaScript
-🔵 twitter: https://twitter.com/cynthiateeters
-🔵 github: https://github.com/cynthiateeters
-🔵 website: https://cynthiateeters.com\n
+${twitter(` Twitter `)} https://twitter.com/cynthiateeters
+${github(` Github  `)} https://github.com/cynthiateeters
+${website(` Website `)} https://cynthiateeters.com\n
 `);
