ec106461e2be33d01b7c9d05c27d404ce448b707
cynthia <glean@cynthiateeters.com>
2021-11-03 22:17:35 -0400

    Add starting new line

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const clearConsole = require('clear-any-console');
 clearConsole();
 
-console.log(`Hi, I'm Cynthia Teeters
+console.log(`
+Hi, I'm Cynthia Teeters
 
 🎓 Adjunct Assistant Professor - Raritan Valley Community College (she/her)
 🏫 I teach web development and JavaScript
