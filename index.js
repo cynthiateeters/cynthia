b8569ca9cfe3e1d8cae0260fddc110df82ef805b
cynthia <glean@cynthiateeters.com>
2021-11-03 22:33:49 -0400

    Add fancy stuff

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
-clearConsole();
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
+
+
+welcome({
+  title: pkgJSON.name,
+  tagLine: `Hey, nice to meet you`,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true,
+});
 
 console.log(`
 Hi, I'm Cynthia Teeters
