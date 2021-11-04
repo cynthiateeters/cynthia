8edd4f85dc73adfeb0ef8b9103210986fadee827
cynthia <glean@cynthiateeters.com>
2021-11-03 21:53:35 -0400

    Add code

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+console.log(`Hi, I'm Cynthia Teeters - Adjunct Assistant Professor
+🎓 Adjunct Assistant Professor - Raritan Valley Community College (she/her)
+🏫 I teach web development and JavaScript
+🔵 twitter: https://twitter.com/cynthiateeters
+🔵 github: https://github.com/cynthiateeters
+🔵 website: https://cynthiateeters.com\n
+`);
